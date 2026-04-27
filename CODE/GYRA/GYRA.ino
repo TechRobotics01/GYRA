1eab7ad2498cd8c02dff44f7b2aea5b150349ca2
Parth Dhiman <parthdhiman.pd@gmail.com>
2026-04-27 22:56:00 +0530

    UPDATED CODE AND CAD FILES

--- CODE/GYRA/GYRA.ino
+++ CODE/GYRA/GYRA.ino
@@ -1,8 +1,9 @@
+#include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <ESP32Servo.h>
 
-const int servo1 = 25;
-const int servo2 = 26;
+const int servo1 = 18;
+const int servo2 = 19;
 
 Servo s1;
 Servo s2;
@@ -16,10 +17,12 @@ Adafruit_Sensor *mpu_ax, *mpu_gr;
 void setup() {
   Serial.begin(115200);
 
+  Wire.begin(21, 22);   // IMPORTANT FIX
+
   s1.attach(servo1);
   s2.attach(servo2);
 
-  Serial.println("WELCOME TO GYRA ");
+  Serial.println("WELCOME TO GYRA");
 
   if (!mpu.begin()) {
     Serial.println("MPU6050 not found");
@@ -55,8 +58,9 @@ void loop() {
   float gz = gr.gyro.z;
   yaw += gz * dt * 180 / PI;
 
-  yaw = map((int)yaw, 0, 360, 0, 255);
-  roll = map((int)roll, 0, 360, 0, 255);
+  // FIXED mapping for servo (0–180)
+  yaw = constrain(yaw, 0, 180);
+  roll = constrain(roll, 0, 180);
 
   Serial.print("ROLL: ");
   Serial.print(roll);
